058afc89b8e843779d0929101fa2d33e5665b905
ConchurOM <61588517+ConchurOM@users.noreply.github.com>
2020-03-03 15:07:50 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UdgNtPoidyfc1RIBtQd9l1dtvqUmm046XIScqAfRGMU/edit#gid=0';
